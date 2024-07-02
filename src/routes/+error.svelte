12a00b72cbf3f35408266e9f2f0c480543fc70a4
zeindevs <zeindevs@gmail.com>
2024-07-02 17:30:31 +0700

    ui: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Footer, Navbar } from '$lib/components/layout';
+</script>
+
+<Navbar />
+
+<header class="px-5 py-20">
+	<div class="container max-w-6xl space-y-5 px-0 text-center">
+		<span class="rounded-full bg-secondary px-2.5 py-1 text-xs font-medium">404</span>
+		<h1 class="text-3xl font-medium capitalize leading-tight">
+			Halaman Tidak Ditemukan
+		</h1>
+	</div>
+</header>
+
+<Footer />
